e724e4eccf07acc37ff22d33d42cbb4e5ccdcbec
Bartosz Ociepka <bociepka@gmail.com>
2020-03-27 20:09:06 +0100

    Completed parsing events from timetable API to TimetableEvent objects.

--- timetable_api/timetable_get.py
+++ timetable_api/timetable_get.py
@@ -1,17 +1,47 @@
 import requests
 import json
-import datetime
+from datetime import datetime
 
 class TimetableEvent:
-    def __init__(self, name, start):
-        self.name = name
+    def __init__(self, title, room, teacher, group, day, start, end, info):
+        self.title = title
+        self.room = room
+        self.teacher = teacher
+        self.group = group
+        self.day = day
         self.start = start
-    def __str__(self):
-        return self.name+" "+str(self.start)
+        self.end = end
+        self.info = info
+
+    def __str__(self) -> str:
+        return self.title+" "+self.room+" "+self.teacher+" "+str(self.group)+" "+str(self.day)+" "+str(self.start)+" "+str(self.end)
+
 
 resp = requests.get("https://planzajec.eaiib.agh.edu.pl/view/timetable/645/events?start=2020-03-02&end=2020-03-07&_=1585327741228")
 dataArray = resp.json()
 
-currentElement = dataArray[0]
-event = TimetableEvent(currentElement.get('title'), currentElement.get('start'))
-print(event)
+dateFormat = '%Y-%m-%dT%H:%M:%S%z'
+
+for i, currentElement in enumerate(dataArray, start = 0):
+    group = str(currentElement.get('group')).replace(".1","a").replace(".2","b")
+    if group == '0':
+        title = currentElement.get('title').split(',')[0]
+        room = currentElement.get('title').split(',')[1].split('<br/>')[1]
+        teacher = currentElement.get('title').split(',')[2]
+        if len(currentElement.get('title').split(',')) >3:
+            teacher += currentElement.get('title').split(',')[3].split('<br/>')[0]
+    else:
+        title = currentElement.get('title').split(',')[0]
+        room = currentElement.get('title').split(',')[2].split('<br/>')[1]
+        teacher = currentElement.get('title').split(',')[3]
+        if len(currentElement.get('title').split(',')) > 3:
+            teacher += currentElement.get('title').split(',')[3].split('<br/>')[0]
+    day = datetime.strptime(currentElement.get('start'), dateFormat).weekday()+1
+    start = datetime.strptime(currentElement.get('start'), dateFormat).time()
+    end = datetime.strptime(currentElement.get('end'), dateFormat).time()
+    info = ""
+    if len(currentElement.get('title').split('<br/>')) == 3:
+        info = currentElement.get('title').split('<br/>')[2]
+    event = TimetableEvent(title, room, teacher, group, day, start, end, info)
+
+    print(i, ": ", event)
