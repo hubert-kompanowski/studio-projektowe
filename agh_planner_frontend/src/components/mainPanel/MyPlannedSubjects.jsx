acdcee8724fffa4c9c89ba22c20636d262aa57b2
Adam Surowka <adam.surowka@pl.abb.com>
2020-07-04 19:34:36 +0200

    Futher frontend changes

---
+++ agh_planner_frontend/src/components/mainPanel/MyPlannedSubjects.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyPlannedSubjects extends Component {
+    render() {
+        return (
+            <div>
+                MyPlannedSubjects
+            </div>
+        );
+    }
+}
+
+export default MyPlannedSubjects;
